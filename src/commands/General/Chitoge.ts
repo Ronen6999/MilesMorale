3975ede874fc3861759d7564accbac0c80e8d2ae
Ronen singha <101617329+Ronen6999@users.noreply.github.com>
2022-04-22 21:32:40 +0530

    Update Chitoge.ts

--- src/commands/General/Chitoge.ts
+++ src/commands/General/Chitoge.ts
@@ -9,7 +9,7 @@ import { ISimplifiedMessage } from "../../typings";
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "chitoge",
+			command: "miles",
 			description: "Displays the info",
 			category: "general",
 			usage: `${client.config.prefix}chitoge`,
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `🌟 *Chitoge* 🌟\n\n🍀 *Description: A WhatsApp Bot With Rich Anime Features.*\n\n🌐 *URL: https://github.com/ShineiIchijo/Chitoge* \n\n 📒 *Guide: https://github.com/ShineiIchijo/Chitoge-Guides* \n`,
+				caption: `🐧 *Miles Morales* 🐧\n\n🍀 *Description: A WhatsApp Bot created by Team Aries and recoded by Ronen With Rich Anime Features.*\n\n🌐 *URL: https://github.com/ShineiIchijo/Chitoge* \n\n 📒 *Guide: https://github.com/ShineiIchijo/Chitoge-Guides* \n`,
 			}
 		);
 	};
